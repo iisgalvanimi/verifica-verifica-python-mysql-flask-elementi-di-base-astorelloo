d8fedef3174d7c8dd3504bee96204bdd7ee92dfc
astorelloo astorelloo <lil.astor06@gmail.com>
2024-11-08 11:11:11 +0000

    punto 3

--- api-rest.py
+++ api-rest.py
@@ -20,11 +20,31 @@ def getAllData():
         result.append(x)
     return result
 @app.route("/")
-
+def home():
+    return('verifica')
 @app.route("/dati")
 def index():
     data = getAllData()
     return jsonify({'Insetti': data})
+
+#punto 3
+def addInsetti(data):
+    query = "INSERT INTO Insetti (nome, famiglia, habitat, alimentazione, caratteristiche) VALUES (%s, %s, %s, %s, %s)"
+    values = (data['nome'], data['famiglia'], data['habitat'], data['alimentazione'],data['caratteristiche'])
+    mycursor.execute(query, values)
+    mydb.commit()
+    return mycursor.rowcount
+
+@app.route("/add", methods=["POST"])
+def add():
+    data = request.json
+    rows_inserted = addInsetti(data)
+    if rows_inserted == 1:
+        return jsonify({'message': 'Insetto inserito con successo'}), 201
+    else:
+        return jsonify({'message': 'Errore durante lo svolgimento della operazione'}), 500
+
+
 
 if __name__ == "__main__":
     app.run()
